6740af37389b3b050c478991c1d937832b29d81e
Dragan Miljkovic <dragan.miljkovic29@gmail.com>
2022-03-08 00:56:13 +0100

    uradjena provera maila i parcijalna provera hasha i odgovarajuc response za issue #12

--- src/backend/Controllers/AuthController.cs
+++ src/backend/Controllers/AuthController.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Mvc;
 using backend.Models;
 using Microsoft.AspNetCore.Http.Extensions;
 using System.Security.Cryptography;
+using Microsoft.Extensions.Configuration;
+using System.Data;
+using Npgsql;
 
 namespace backend.Controllers
 {
@@ -17,17 +20,70 @@ namespace backend.Controllers
     {
 
          static User u = new User();
+        Response res = new Response();
+
+        private readonly IConfiguration _cofiguraion;
         
-        public AuthController()
+        public AuthController(IConfiguration configuration)
         {
+            _cofiguraion = configuration;
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<User>> Register() {
+        public async Task<ActionResult<Response>> Register(userReg register) {
 
-            return Ok("poruke okej je");
+            res.Success = true;
+            //proveravanje maila
+            
+            if (!IsValidEmail(register.Email))
+            {
+                res.Success = false;
+                res.errors.Add(new Error("incorrect_email", "Incorrect email address!"));
+            }
+            //proveravanje hasha
+            if(register.HashedPassword.Length!=60)
+            {
+                res.Success = false;
+                res.errors.Add(new Error("incorrect_hash_length", "Hash length doesn't match!"));
+            }
+            if(res.Success==false)
+            {
+                return BadRequest(res);
+            }
+            //proveravanje sql injectiona
+
+            
+
+            return Ok(register);
         }   
 
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select * from users";
+            List<User> users = new List<User>();
+            string SqlDataSource = _cofiguraion.GetConnectionString("DefaultConnection");
+            NpgsqlDataReader reader;
+            using (NpgsqlConnection conn = new NpgsqlConnection(SqlDataSource))
+            {
+                conn.Open();
+                using(NpgsqlCommand command=new NpgsqlCommand(query, conn))
+                {
+                    reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        u.Id=reader.GetInt32("id");
+                        u.Username=reader.GetString("username");
+                        u.Email=reader.GetString("email");
+                        u.PasswordHashed=reader.GetString("passwordhashed");
+                        users.Add(u);
+                    }
+                    reader.Close();
+                    conn.Close();
+                }
+            }
+            return new JsonResult(users);
+        }
 
 
         [HttpPost("login")]
@@ -60,6 +116,24 @@ namespace backend.Controllers
             }
         }
 
-        
+        private bool IsValidEmail(string email)
+        {
+            var trimmedEmail = email.Trim();
+
+            if (trimmedEmail.EndsWith("."))
+            {
+                return false; // suggested by @TK-421
+            }
+            try
+            {
+                var addr = new System.Net.Mail.MailAddress(email);
+                return addr.Address == trimmedEmail;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
     }
 }
---
+++ src/backend/Error.cs
@@ -0,0 +1,14 @@
+﻿namespace backend
+{
+    public class Error
+    {
+        public Error(string code, string message)
+        {
+            Code = code;
+            Message = message;
+        }
+
+        private String Code { get; set; }
+        private String Message { get; set; }
+    }
+}
