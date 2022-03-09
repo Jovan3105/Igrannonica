98558bef4ed625909bf992937daba0c0589ff50a
Srdjan Todorovic <tsrdjan@pm.me>
2022-03-09 22:41:58 +0100

    Dodat testing projekat
    Dodati front i back za testiranje
    Issue: #18

---
+++ sandbox/testing/backend/Response.cs
@@ -0,0 +1,12 @@
+﻿namespace backend
+{
+    public class Response
+    {
+        public bool Success { get; set; } = true;
+
+        public List<Error> Errors { get => errors; }
+
+        public List<Error> errors = new List<Error>();
+
+    }
+}
