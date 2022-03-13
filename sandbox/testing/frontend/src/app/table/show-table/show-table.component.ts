56a4265191af132a114599a736449c877d07c859
Dragan Miljkovic <dragan.miljkovic29@gmail.com>
2022-03-13 19:55:57 +0100

    csv citac fajla (#24)
    Former-commit-id: 269b07a3b45df725561edc0b3aed921ad65dcf19

---
+++ sandbox/testing/frontend/src/app/table/show-table/show-table.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+import { Papa, ParseResult } from 'ngx-papaparse';
+import { ColDef,GridApi,GridReadyEvent,CellValueChangedEvent } from 'ag-grid-community';
+
+@Component({
+  selector: 'app-show-table',
+  templateUrl: './show-table.component.html',
+  styleUrls: ['./show-table.component.css']
+})
+export class ShowTableComponent implements OnInit {
+
+  headers:any[] = [];
+  data:any = null;
+  private gridApi!: GridApi;
+
+  constructor(private papa:Papa) { }
+
+  columnDefs: ColDef[] = [];
+  rowData:any = [];
+  public rowSelection = 'multiple';
+  public paginationPageSize = 10;
+
+  ngOnInit(): void {
+  }
+
+  onFileSelected(event:Event)
+  {
+    const element = event.currentTarget as HTMLInputElement;
+    let fileList: FileList | null = element.files;
+    
+    if (fileList && fileList?.length > 0) 
+    {
+      var file = fileList[0];
+      var parseResult : ParseResult = this.papa.parse(file,{
+        header: true,
+        skipEmptyLines:true,
+        //download:true,
+        complete: (results) =>
+        {
+          this.data = results.data
+          this.prepareTable()
+        }
+      });
+    }
+  }
+
+  prepareTable()
+  {
+    if (this.data.length > 0) this.headers = Object.getOwnPropertyNames(this.data[0]); 
+    this.columnDefs = [];
+    this.rowData = [];
+    for(let header of this.headers)
+    {
+      var col = {
+        flex: 1,
+        field: header,
+        sortable: true,
+        filter: true,
+        editable: true,
+        resizable:true,
+        minWidth: 100
+      }
+      this.columnDefs.push(col);
+    }
+
+    for(let row of this.data)
+    {
+      this.rowData.push(row);
+    }
+  }
+
+  onGridReady(params: GridReadyEvent) {
+    this.gridApi = params.api;
+  }
+
+  onRemoveSelected() {
+    const selectedData = this.gridApi.getSelectedRows();
+    const res = this.gridApi.applyTransaction({ remove: selectedData })!;
+    
+    for(let sData of selectedData)
+    {
+      var index = this.data.indexOf(sData,0);
+      if (index != -1) this.data.splice(index,1);
+    }
+  }
+}
