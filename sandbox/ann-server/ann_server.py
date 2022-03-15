7ef8a1859f209d6a756e0b0c06b45a66fc143d23
Srdjan Todorovic <tsrdjan@pm.me>
2022-03-15 23:24:23 +0100

    Testirana implementacija Flask_RESTful server-a
    Testirana implementacija Flask RESTful servera u sandbox-u
    Issue: #25
    
    Former-commit-id: fbe07d2235b4cfe70610a9f86e868c71a4ab5d27

---
+++ sandbox/ann-server/ann_server.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_restful import Resource, Api
+from controllers.dataprep import *
+from controllers.training import *
+
+
+app = Flask(__name__)
+api = Api(app)
+
+hostName = "localhost"
+serverPort = 8081
+
+#################################################################
+# Resources & routes
+
+api.add_resource(DataPreparation, '/dataprep')
+api.add_resource(Training, '/training/<int:tid>')
+
+#################################################################
+
+if __name__ == "__main__":        
+    app.run(
+        host=hostName, 
+        port=serverPort, 
+        threaded=True, # thread per request
+        debug=True)
+    print("Server started http://%s:%s" % (hostName, serverPort))
