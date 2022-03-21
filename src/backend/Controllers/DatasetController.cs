a76f88d5e0996c9a2dad6f5e27dc020ad8133b8a
Jovan Brzakovic <jova3105@gmail.com>
2022-03-21 17:19:28 +0100

    Kreirane komponente za prikaz podataka Issue: #43
    Former-commit-id: 0569f3ea6e7e37e1469d1536152a0427ad7adbbd

--- src/backend/Controllers/DatasetController.cs
+++ src/backend/Controllers/DatasetController.cs
@@ -129,7 +129,7 @@ namespace backend.Controllers
             Console.WriteLine(text);
             Console.WriteLine(fileName);
 
-            var filePath = @"C:\Users\Pivan\Documents\";
+            var filePath = @"C:\Users\jovan\Documents\";
 
 
 
