79d2306ce03056d0634cf2fc264c459d4ca2be5a
Srdjan Todorovic <tsrdjan@pm.me>
2022-03-24 00:05:43 +0100

    Merge remote branch 'origin/refresh-token-back` into dev
    Former-commit-id: 5e2df3e753502e6191a3fe10f4b6b5a539b45e54

--- src/backend/Controllers/AuthController.cs
+++ src/backend/Controllers/AuthController.cs
@@ -12,6 +12,8 @@ using BCrypt.Net;
 using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
 
 namespace backend.Controllers
 {
@@ -63,7 +65,7 @@ namespace backend.Controllers
 
 
 
-            string token = CreateJWT(user);
+            
 
             //slanje verifikacionog mejla
 
@@ -105,15 +107,32 @@ namespace backend.Controllers
                 user.Username = "prosaoCheck";
                 this.userContext.Update(user);
                 await this.userContext.SaveChangesAsync();
-                string token = CreateJWT(user);
+                List<Claim> claims = new List<Claim>
+                     {
+                            new Claim(ClaimTypes.Name,user.Username),
+                            new Claim(ClaimTypes.Email,user.Email),
+                             new Claim(ClaimTypes.SerialNumber,user.Id.ToString()),
+                            new Claim("message","Logging in..."),
+
+
+
+
+                         };
+               
+                JwtSecurityToken token = CreateToken(claims);
+
                 return Ok(new
                 {
                     success = true,
                     data = new
                     {
-                        token = token,
+                        token = new JwtSecurityTokenHandler().WriteToken(token),
+
+
+
+
                     }
-                }) ;
+                });
             }    
         }
 
@@ -166,14 +185,38 @@ namespace backend.Controllers
             {
                 if (BCrypt.Net.BCrypt.Verify(request.Password, user.PasswordHashed))
                 {
-                    string token = CreateJWT(user);
+
+                    List<Claim> claims = new List<Claim>
+                     {
+                            new Claim(ClaimTypes.Name,user.Username),
+                            new Claim(ClaimTypes.Email,user.Email),
+                             new Claim(ClaimTypes.SerialNumber,user.Id.ToString()),
+
+                            new Claim("message","Logging in..."),
+                            
+
+
+
+                         };
+                    var refreshToken = GenerateRefreshToken();
+                    JwtSecurityToken token = CreateToken(claims);
+                    user.RefreshToken = refreshToken;
+                    _ = int.TryParse(_configuration["JWT:RefreshTokenValidityInDays"], out int refreshTokenValidityInDays);
+                    user.RefreshTokenExpires = DateTime.Now.AddDays(refreshTokenValidityInDays);
+
+
+
+
+                    userContext.Entry(user).State = EntityState.Modified;
+                    await userContext.SaveChangesAsync();
                     return Ok(new
                     {
                         success = true,
                         data = new
                         {
-                            token = token,
-                           
+                            token = new JwtSecurityTokenHandler().WriteToken(token),
+                            refreshToken= refreshToken
+
 
 
 
@@ -206,50 +249,134 @@ namespace backend.Controllers
 
            
         }
+<<<<<<< HEAD
         
    
         private string CreateJWT(User user)
+=======
+        [HttpGet("users")]
+        public async Task<ActionResult<List<User>>> getUsers()
         {
-            List<Claim> claims = new List<Claim>
-            {
-                new Claim(ClaimTypes.Name,user.Username),
-                new Claim(ClaimTypes.Email,user.Email),
-                new Claim(ClaimTypes.SerialNumber,user.Id.ToString()),
-                new Claim("message","Logging in..."),
-                new Claim("access_token", CreateAccessJWT())
-
-
-
-            };
-            var key = new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
-            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-            var token = new JwtSecurityToken(
-                claims: claims,
-                expires:DateTime.Now.AddDays(1),
-                signingCredentials: creds
+            return Ok(await this.userContext.Users.ToListAsync());
+        }
                 
-                );
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-
-            return jwt;
-        }
-        private string CreateAccessJWT()
+        private void CreatePasswordHash(string password,out byte[] passwordHash,out byte[] passwordSalt)
         {
-            var key= new SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value));
+            using(var hmac =  new HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash= hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+
+            }
+        }     
+        private bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using (var hmac = new HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);  
+
+            }
+        }
+       
+        
+        private JwtSecurityToken CreateToken(List<Claim> authClaims)
+>>>>>>> origin/refresh-token-back
+        {
+
+
+
+
+            var authSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["AppSettings:Token"]));
+            _ = int.TryParse(_configuration["JWT:TokenValidityInMinutes"], out int tokenValidityInMinutes);
 
             var token = new JwtSecurityToken(
-                expires: DateTime.Now.AddDays(1),
-                signingCredentials: new SigningCredentials(key, SecurityAlgorithms.HmacSha512)
+                issuer: _configuration["JWT:ValidIssuer"],
+                audience: _configuration["JWT:ValidAudience"],
+                expires: DateTime.Now.AddMinutes(tokenValidityInMinutes),
+                claims: authClaims,
+                signingCredentials: new SigningCredentials(authSigningKey, SecurityAlgorithms.HmacSha256)
+                );
 
-
-
-                ) ;
-            
-            
-            var jwt = new JwtSecurityTokenHandler().WriteToken(token);
-            return jwt;
+            return token;
         }
+        [Authorize]
+        [HttpPost]
+        [Route("refresh-token")]
+        public async Task<IActionResult> RefreshToken(TokenModel tokenModel)
+        {
+            if (tokenModel is null)
+            {
+                return BadRequest("Invalid client request");
+            }
+
+            string? accessToken = tokenModel.AccessToken;
+            string? refreshToken = tokenModel.RefreshToken;
+
+            var principal = GetPrincipalFromExpiredToken(accessToken);
+            if (principal == null)
+            {
+                return BadRequest("Invalid access token or refresh token");
+            }
+
+
+            string username = principal.Identity.Name;
+            
+
+
+            var user = this.userContext.Users.FirstOrDefault(x=>x.Username==username);
+
+            if (user == null || user.RefreshToken != refreshToken || user.RefreshTokenExpires <= DateTime.Now)
+            {
+                return BadRequest("Invalid access token or refresh token");
+            }
+
+            var newAccessToken = CreateToken(principal.Claims.ToList());
+            var newRefreshToken = GenerateRefreshToken();
+
+            user.RefreshToken = newRefreshToken;
+
+
+
+            userContext.Entry(user).State = EntityState.Modified;
+            await userContext.SaveChangesAsync();
+
+
+            return new ObjectResult(new
+            {
+                accessToken = new JwtSecurityTokenHandler().WriteToken(newAccessToken),
+                refreshToken = newRefreshToken
+            });
+        }
+        private ClaimsPrincipal? GetPrincipalFromExpiredToken(string? token)
+        {
+            Console.WriteLine("\n\n\n\n\n pozvano");
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JWT:Secret"])),
+                ValidateLifetime = false
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
+            if (securityToken is not JwtSecurityToken jwtSecurityToken || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+                throw new SecurityTokenException("Invalid token");
+
+            return principal;
+
+        }
+        private static string GenerateRefreshToken()
+        {
+            var randomNumber = new byte[64];
+            using var rng = RandomNumberGenerator.Create();
+            rng.GetBytes(randomNumber);
+            return Convert.ToBase64String(randomNumber);
+        }
+
 
         private bool IsValidEmail(string email)
         {
--- src/backend/Program.cs
+++ src/backend/Program.cs
@@ -1,10 +1,17 @@
 using backend.Data;
+using backend.Models;
 using backend.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
 
 
 var MyAllowSpecificOrigins = "MyAllowSpecificOrigins";
+
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddCors(options =>
@@ -16,7 +23,7 @@ builder.Services.AddCors(options =>
                                                   .AllowAnyMethod();
                       });
 });
-
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -31,6 +38,34 @@ builder.Services.AddDbContext<DatasetContext>(options => {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 });
+//builder.Services.AddIdentity<User, IdentityRole>().AddEntityFrameworkStores<UserContext>().AddDefaultTokenProviders();
+
+
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero,
+
+        ValidAudience = configuration["JWT:ValidAudience"],
+        ValidIssuer = configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
+
+    };
+});
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -50,7 +85,11 @@ app.UseHttpsRedirection();
 
 app.UseCors(MyAllowSpecificOrigins);
 
+
+
+app.UseAuthentication();
 app.UseAuthorization();
+
 
 app.MapControllers();
 
