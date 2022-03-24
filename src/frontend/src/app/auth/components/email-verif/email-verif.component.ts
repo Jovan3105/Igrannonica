fe41fc3801e91eeae0a2a7538be1de6e71265b4f
Srdjan Todorovic <tsrdjan@pm.me>
2022-03-24 12:21:19 +0100

    Dodati fajlovi koji se fale za email verif fix
    Issue: #53
    
    Former-commit-id: 9ef63c0c6daed8374d5eb4bc54577ebcd907d70a

---
+++ src/frontend/src/app/auth/components/email-verif/email-verif.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
+
+@Component({
+  selector: 'app-email-verif',
+  templateUrl: './email-verif.component.html',
+  styleUrls: ['./email-verif.component.css']
+})
+export class EmailVerifComponent implements OnInit {
+
+  constructor(private authService: AuthService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    const emailVerifObserver = {
+      next: (x:any) => { 
+        console.log('Email is verified'); 
+        this.router.navigateByUrl('/api/login'); 
+      },
+      error: (err: Error) => {
+        console.log(err)
+
+      }
+    };
+
+    const verifRequestObserver = {
+      next: (params:any) => { 
+        console.log('Verification link params:');
+        console.log(params); 
+        this.authService.verifyEmailAddress(params['params']['email'], params['params']['token']).subscribe(emailVerifObserver);
+      },
+      error: (err: Error) => {
+        console.log(err)
+
+      }
+    };
+    
+    this.route.queryParamMap.subscribe(verifRequestObserver);
+        
+  }
+
+}
