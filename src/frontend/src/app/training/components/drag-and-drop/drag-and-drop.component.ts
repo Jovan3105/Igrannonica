cdfbc65abea8e5f61f8187d1d7062611e87dcc99
Dragan Miljkovic <dragan.miljkovic29@gmail.com>
2022-04-20 02:33:41 +0200

    dodata funkcionalnost i dodatna poboljsavanja #108
    Former-commit-id: aa55090609f6c2138c0250a1fd86f7302ff3e72f

--- src/frontend/src/app/training/components/drag-and-drop/drag-and-drop.component.ts
+++ src/frontend/src/app/training/components/drag-and-drop/drag-and-drop.component.ts
@@ -1,23 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { CdkDragDrop, moveItemInArray} from '@angular/cdk/drag-drop';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
 @Component({
   selector: 'app-drag-and-drop',
   templateUrl: './drag-and-drop.component.html',
   styleUrls: ['./drag-and-drop.component.css']
 })
+
+
 export class DragAndDropComponent implements OnInit {
 
   constructor() { }
-  timePeriods = [
-    'Bronze age',
-    'Iron age',
-    'Middle ages',
-    'Early modern period',
-    'Long nineteenth century',
+  layers= [
+    {"units":4,"af":"ReLu"},
+    {"units":7,"af":"ReLu"},
+    {"units":1,"af":"ReLu"}
   ];
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.layers, event.previousIndex, event.currentIndex);
+  }
+
+  removeLayer(index:number){
+
+    this.layers.splice(index, 1);
+
+  }
+
+  addLayer(){
+    this.layers.push({"units":1,"af":"ReLu"});
   }
 
   ngOnInit(): void {
