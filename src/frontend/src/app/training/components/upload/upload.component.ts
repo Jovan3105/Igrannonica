181c57c4acd9cbce73af3403dabde5c832189360
Jovan Brzakovic <jova3105@gmail.com>
2022-05-20 22:24:56 +0200

    Sitne izmene css kod hiperparametara, dodati neki defaultni datasetovi kod upload stranice
    Former-commit-id: d63cb4fad319f40ffa6613bc232dd01a019f6fa7

--- src/frontend/src/app/training/components/upload/upload.component.ts
+++ src/frontend/src/app/training/components/upload/upload.component.ts
@@ -15,6 +15,13 @@ export class UploadComponent implements OnInit {
   datasetURL:string;
   isLoggedIn:boolean;
   showDragAndDrop:boolean;
+
+  diamondsURL:string;
+  titanicURL:string;
+  weightsURL:string;
+  covidURL:string;
+  browserURL:string;
+
   @Output() linkEvent: EventEmitter<string>; //podizanje event-a kada se salje link
   @Output() uploadEvent: EventEmitter<File>; //podizanje event-a kada se salje file
 
@@ -24,6 +31,11 @@ export class UploadComponent implements OnInit {
     this.uploadEvent = new EventEmitter<File>();
     this.isLoggedIn = this.authService.isLoggedIn();
     this.showDragAndDrop = true;
+    this.diamondsURL = "https://raw.githubusercontent.com/tidyverse/ggplot2/main/data-raw/diamonds.csv"
+    this.titanicURL = "https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv";
+    this.weightsURL = "https://raw.githubusercontent.com/kintan-pitaloka/weight-height-dataset/main/weight-height.csv";
+    this.covidURL = "https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv";
+    this.browserURL = "https://raw.githubusercontent.com/datasets/browser-stats/master/data.csv";
    }
 
   ngOnInit(): void 
@@ -66,5 +78,10 @@ export class UploadComponent implements OnInit {
   {
     this.file = undefined;
     this.showDragAndDrop = true;
+  }
+  publicLinkClick(datasetLink:string)
+  {
+    this.linkEvent.emit(datasetLink);
+    this.fileName = datasetLink.split("/").pop();
   }
 }
